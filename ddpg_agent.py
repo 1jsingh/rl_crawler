cfe50e7e235e8450c7700bbdeb934f969f104dc6
1jsingh <1.jaskiratsingh@gmail.com>
2018-12-16 09:33:06 +0000

    update

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -12,7 +12,7 @@ import torch.optim as optim
 BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 512        # minibatch size
 GAMMA = 0.99           # discount factor
-TAU = 1e-2              # for soft update of target parameters
+TAU = 3e-2              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor 
 LR_CRITIC = 1e-3        # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
